060cb67e52a2a382ae4d5dff6ee6025af8d42927
AlexVipond <alex.r.vipond@gmail.com>
2020-03-17 23:02:39 -0600

    Add material design/Tailwind timings

---
+++ README.md
@@ -0,0 +1,5 @@
+# `@baleada/animateable-timings`
+
+Easing curve control points, pre-formatted so they can be passed to `Animateable`'s `timing` option.
+
+[See the Baleada docs](https://baleada/netlify.com) for more guidance.
---
+++ index.js
@@ -0,0 +1,15 @@
+// Material Design and Tailwind
+export const materialStandard = [
+  { x: 0.4, y: 0.0 },
+  { x: 0.2, y: 1.0 },
+]
+
+export const materialDecelerated = [
+  { x: 0.0, y: 0.0 },
+  { x: 0.2, y: 1.0 },
+]
+
+export const materialAccelerated = [
+  { x: 0.4, y: 0.0 },
+  { x: 1.0, y: 1.0 },
+]
