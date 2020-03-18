6a4f3c11771c4d1f12bb622dc2e34e2b2b44d8b2
AlexVipond <alex.r.vipond@gmail.com>
2020-03-17 23:32:54 -0600

    Add verou easings

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
+export const linear = [
+  { x: 0.00, y: 0.00 },
+  { x: 1.00, y: 1.00 },
+]
+
 // Material Design and Tailwind
 export const materialStandard = [
-  { x: 0.4, y: 0.0 },
-  { x: 0.2, y: 1.0 },
+  { x: 0.40, y: 0.00 },
+  { x: 0.20, y: 1.00 },
 ]
-
 export const materialDecelerated = [
-  { x: 0.0, y: 0.0 },
-  { x: 0.2, y: 1.0 },
+  { x: 0.00, y: 0.00 },
+  { x: 0.20, y: 1.00 },
+]
+export const materialAccelerated = [
+  { x: 0.40, y: 0.00 },
+  { x: 1.00, y: 1.00 },
 ]
 
-export const materialAccelerated = [
-  { x: 0.4, y: 0.0 },
-  { x: 1.0, y: 1.0 },
+// Lea Verou of https://cubic-bezier.com/ fame
+export const verouEase = [
+  { x: 0.25, y: 0.10 },
+  { x: 0.25, y: 1.00 },
+]
+export const verouEaseIn = [
+  { x: 0.42, y: 0.00 },
+  { x: 1.00, y: 1.00 },
+]
+export const verouEaseOut = [
+  { x: 0.00, y: 0.00 },
+  { x: 0.58, y: 1.00 },
+]
+export const verouEaseInOut = [
+  { x: 0.42, y: 0.00 },
+  { x: 0.58, y: 1.00 },
 ]
