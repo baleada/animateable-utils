741673c2a6bdbf02196fe0f00abfd9dd3de866ac
AlexVipond <alex.r.vipond@gmail.com>
2020-03-18 10:59:18 -0600

    add easings.net

--- index.js
+++ index.js
@@ -1,36 +1,126 @@
 export const linear = [
-  { x: 0.00, y: 0.00 },
-  { x: 1.00, y: 1.00 },
+  0   , 0   ,
+  1   , 1   ,
 ]
 
 // Material Design and Tailwind
 export const materialStandard = [
-  { x: 0.40, y: 0.00 },
-  { x: 0.20, y: 1.00 },
+  0.4 , 0   ,
+  0.2 , 1   ,
 ]
 export const materialDecelerated = [
-  { x: 0.00, y: 0.00 },
-  { x: 0.20, y: 1.00 },
+  0   , 0   ,
+  0.2 , 1   ,
 ]
 export const materialAccelerated = [
-  { x: 0.40, y: 0.00 },
-  { x: 1.00, y: 1.00 },
+  0.4 , 0   ,
+  1   , 1   ,
 ]
 
 // Lea Verou of https://cubic-bezier.com/ fame
 export const verouEase = [
-  { x: 0.25, y: 0.10 },
-  { x: 0.25, y: 1.00 },
+  0.25, 0.1 ,
+  0.25, 1   ,
 ]
 export const verouEaseIn = [
-  { x: 0.42, y: 0.00 },
-  { x: 1.00, y: 1.00 },
+  0.42, 0   ,
+  1   , 1   ,
 ]
 export const verouEaseOut = [
-  { x: 0.00, y: 0.00 },
-  { x: 0.58, y: 1.00 },
+  0   , 0   ,
+  0.58, 1   ,
 ]
 export const verouEaseInOut = [
-  { x: 0.42, y: 0.00 },
-  { x: 0.58, y: 1.00 },
+  0.42, 0   ,
+  0.58, 1   ,
+]
+
+// easings.net
+export const easingsNetInSine = [
+  0.12, 0   ,
+  0.39, 0   ,
+]
+export const easingsNetOutSine = [
+  0.61, 1   ,
+  0.88, 1   ,
+]
+export const easingsNetInOutSine = [
+  0.37, 0   ,
+  0.63, 1   ,
+]
+export const easingsNetInQuad = [
+  0.11, 0   ,
+  0.5 , 0   ,
+]
+export const easingsNetOutQuad = [
+  0.5 , 1   ,
+  0.89, 1   ,
+]
+export const easingsNetInOutQuad = [
+  0.45, 0   ,
+  0.55, 1   ,
+]
+export const easingsNetInCubic = [
+  0.32, 0   ,
+  0.67, 0   ,
+]
+export const easingsNetOutCubic = [
+  0.33, 1   ,
+  0.68, 1   ,
+]
+export const easingsNetInOutCubic = [
+  0.65, 0   ,
+  0.35, 1   ,
+]
+export const easingsNetInQuart = [
+  0.5 , 0   ,
+  0.75, 0   ,
+]
+export const easingsNetInQuint = [
+  0.64, 0   ,
+  0.78, 0   ,
+]
+export const easingsNetOutQuint = [
+  0.22, 1   ,
+  0.36, 1   ,
+]
+export const easingsNetInOutQuint = [
+  0.83, 0   ,
+  0.17, 1   ,
+]
+export const easingsNetInExpo = [
+  0.7 , 0   ,
+  0.84, 0   ,
+]
+export const easingsNetOutExpo = [
+  0.16, 1   ,
+  0.3 , 1   ,
+]
+export const easingsNetInOutExpo = [
+  0.87, 0   ,
+  0.13, 1   ,
+]
+export const easingsNetInCirc = [
+  0.55, 0   ,
+  1   , 0.45,
+]
+export const easingsNetOutCirc = [
+  0   , 0.55,
+  0.45, 1   ,
+]
+export const easingsNetInOutCirc = [
+  0.85, 0   ,
+  0.15, 1   ,
+]
+export const easingsNetInBack = [
+  0.36, 0   ,
+  0.66,-0.56,
+]
+export const easingsNetOutBack = [
+  0.34, 1.56,
+  0.64, 1   ,
+]
+export const easingsNetInOutBack = [
+  0.68,-0.6 ,
+  0.32, 1.6 ,
 ]
